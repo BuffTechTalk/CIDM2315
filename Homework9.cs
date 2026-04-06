e388e973b66763526cbe58cceb3485256a5983fb
BuffTechTalk <tlopez2@buffs.wtamu.edu>
2026-04-05 20:54:58 -0500

    Upload Homework9 file

--- Homework9.cs
+++ Homework9.cs
@@ -6,8 +6,9 @@ namespace Homework9;
 class Program
 {
     static void Main(string[] args)
+    //setting up the dictionary for student names and gpa's
     {
-        Dictionary<string, double> gradebook = new Dictionary<string, double>()
+        var gradebook = new Dictionary<string, double>
         {
         
         {"Alice", 4.0},
@@ -17,47 +18,44 @@ class Program
         };
         gradebook.Add("Tom", 3.3);
 
-        Student.studentList.Add(new Student(111, "Alice"));
-        Student.studentList.Add(new Student(222, "Bob"));
-        Student.studentList.Add(new Student(333, "Cathy"));
-        Student.studentList.Add(new Student(444, "David"));
-        Student.studentList.Add(new Student(555, "Tom"));
-
+        List<Student> students = new List<Student>
+        {
+        
+        //creating each student to a list with id's and name
+        new Student(111, "Alice"),
+        new Student(222, "Bob"),
+        new Student(333, "Cathy"),
+        new Student(444, "David"),
+        new Student(555, "Tom")
+        };
+        
+        //method to average out all gpa's from students
         double average = gradebook.Values.Average();
         Console.WriteLine($"The average GPA of all students is: {average}\n");
+        
+        //setting up properties for printing out results
+        var aboveAverage = students.Join(gradebook, s => s.StudentName, g => g.Key, (s, g) => new {s.StudentID, s.StudentName, GPA = g.Value}).Where(item => item.GPA > average);
 
-        var studentGrades = Student.studentList.Join(gradebook, student => student.GetName(), grade => grade.Key, (student, grade) => new 
+            //taking gpa's from students and printing out result that's above average
+            Console.WriteLine($"The students whose GPA is above average are:");
+            foreach(var student in aboveAverage)
         {
-            ID = student.GetID(),
-            Name = student.GetName(),
-            GPA = grade.Value
-        }
-        );
-
-        Console.WriteLine($"The students whose GPA is above average are:");
-        foreach (var item in studentGrades)
-        {
-            if (item.GPA > average)
-            {
-                Console.WriteLine($"Student ID: {item.ID}, Student Name: {item.Name}");
-            }
+            Console.WriteLine($"Student ID: {student.StudentID}, Student Name: {student.StudentName}");
         }
     }
 }
+//creating student class and it's properties with public and private settings
 class Student
 {
-    public static List<Student> studentList = new List<Student>();
-    private int studentID;
-    private string studentName;
+    public int StudentID {get; private set;}
+    public string StudentName {get; private set;}
     public Student(int id, string name)
     {
-        studentID = id;
-        studentName = name;
+        StudentID = id;
+        StudentName = name;
     }
-    public string GetName() => studentName;
-    public int GetID() => studentID;
-    public void PrintInfo()
-    {
-        Console.WriteLine($"Student ID: {studentID}, Student Name: {studentName}");
-    }
+    //method used to print out results on above average student name and id
+    public void PrintInfo() =>
+    
+        Console.WriteLine($"Student ID: {StudentID}, Student Name: {StudentName}");
 }
